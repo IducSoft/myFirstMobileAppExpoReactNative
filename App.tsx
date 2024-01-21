71d55db0b75af508e3948847cc8545a3fe5451e4
Isaac Urdaneta <urdanetacuarteisaacdavid@gmail.com>
2024-01-21 09:33:04 -0400

    working yet

--- App.tsx
+++ App.tsx
@@ -3,14 +3,13 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Timer from './src/views/Timer';
 
-
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer >
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={Home} />
+      <Stack.Navigator initialRouteName="Home" >
+        <Stack.Screen name="Home" component={Home}/>
         <Stack.Screen name="Timer" component={Timer} />
       </Stack.Navigator>
     </NavigationContainer>
